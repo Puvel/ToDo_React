2adbc78d1c2dd12c993bb05595205f927100629d
Puvel <puvelko@gmail.com>
2020-06-02 13:07:10 +0300

    basic

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './stylesheet/libs/normalize.css';
+import './stylesheet/fonts.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
