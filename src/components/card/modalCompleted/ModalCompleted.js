8b2cbcb0687a7ab391eac560db97023711982f65
Puvel <puvelko@gmail.com>
2020-06-10 14:33:15 +0300

    add complited

--- src/components/card/modalCompleted/ModalCompleted.js
+++ src/components/card/modalCompleted/ModalCompleted.js
@@ -1,21 +1,20 @@
-import React from "react";
-import styles from "./modalCompleted.module.css";
+import React from 'react';
+import styles from './modalCompleted.module.css';
 
 const ModalCompleted = ({ name, handleContinuteDone }) => {
   return (
     <div>
       <div>
         <h2 className={styles.completeModalTitle}>
-          Ceompleted:
+          Completed:
           <span className={styles.completeModalTitleSpan}>{name}</span>
         </h2>
         <div>
-          {/* <button onClick={() => {}}>CANCEL</button> */}
           <div className={styles.completeModalImage}></div>
           <button
             className={styles.completeModalText}
             onClick={handleContinuteDone}>
-            Continue →
+            Continue→
           </button>
         </div>
       </div>
